74dafec14fadba386785ebf53b34d44f8c4589fc
qyl <553579048@qq.com>
2022-07-14 18:09:58 +0800

    create pro

---
+++ src/main/java/com/example/calinterface/utils/ExlUtils.java
@@ -0,0 +1,10 @@
+package com.example.calinterface.utils;
+
+/**
+ * @author qyl
+ * @program ExlUtils.java
+ * @Description TODO
+ * @createTime 2022-07-14 17:57
+ */
+public class ExlUtils {
+}
---
+++ src/test/java/com/example/calinterface/CalInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
